f2468182550de898c5fe6dfb4287b709e3495857
Leroy linyonyi Lusenaka <106273984+lusenakalee@users.noreply.github.com>
2026-05-02 18:32:18 +0000

    text reveal on portfolio

--- components/portfolio/TextBlockEffectDemo.tsx
+++ components/portfolio/TextBlockEffectDemo.tsx
@@ -2,15 +2,15 @@ import TextBlockEffect, { TextBlock } from '@/components/ui/TextBlockEffect';
 
 const TextBlockEffectDemo = () => (
     <TextBlockEffect>
-        <section className="min-h-screen flex items-center justify-center">
+        <section className="h-1/2 md:min-h-1/2 py-8 flex items-center justify-center">
             <TextBlock>
-My name is Warren Gathigi Kamau, and this is the official showcase of my thought bubble, Man Serif.Think .            </TextBlock>
+                My name is Warren Gathigi Kamau, and this is the official showcase of my thought bubble, Man Serif.Think .            </TextBlock>
         </section>
-         <section className="min-h-screen flex items-center justify-center">
-  <TextBlock>
-Now, we begin a new journey, I don't know where or how far this will go, but I trust that God is with me in the beginning, and so I am sure of it's works completion.  </TextBlock>
-</section>
- 
+        <section className="h-1/2 md:min-h-1/2 py-8 md:py-12 flex items-center justify-center">
+            <TextBlock>
+                Now, we begin a new journey, I don't know where or how far this will go, but I trust that God is with me in the beginning, and so I am sure of it's works completion.  </TextBlock>
+        </section>
+
     </TextBlockEffect>
 );
 
